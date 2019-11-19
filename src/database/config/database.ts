b6e25dd87267dfc5e282b9a29fd144fad89c68de
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2019-11-19 20:49:22 +0100

    Fix DB Config error

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,13 +1,4 @@
 import Sequelize from 'sequelize';
 import logger from '../../helpers/logger';
-//import {production, staging, development} from './config';
 
-/*
-const env = process.env.NODE_ENV || 'development';
-let config;
-if (env === 'production') config = production;
-else config = staging;
-*/
-//export = new Sequelize.Sequelize(config.url, { logging: msg => logger.info(`Call to DB : ${msg}`) });
-//export = new Sequelize.Sequelize('postgres://'+process.env.MCQ_DATABASE_USER+':'+process.env.MCQ_DATABASE_PASSWORD+'@127.0.0.1:5432/mcqservice');
-export = new Sequelize.Sequelize(process.env.DATABASE_URL!);
+export = new Sequelize.Sequelize(process.env.DATABASE_URL!, { logging: msg => logger.info(`Call to DB : ${msg}`) });
